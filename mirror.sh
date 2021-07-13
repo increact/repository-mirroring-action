2b0a25bab6c644ddb074290ffb21482886ec8f4c
Tik <tikman@increact.com>
2021-07-13 17:58:36 +0800

    Feat: Update mirror 3 branches

--- mirror.sh
+++ mirror.sh
@@ -5,4 +5,6 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
+git push --tags --force --prune mirror "refs/remotes/origin/develop:refs/heads/develop"
+git push --tags --force --prune mirror "refs/remotes/origin/master:refs/heads/master"
+git push --tags --force --prune mirror "refs/remotes/origin/feature-testing:refs/heads/feature-testing"
