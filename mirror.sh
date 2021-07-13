762706e882816ba069991b579417c2235b885060
Tik <tikman@increact.com>
2021-07-13 18:14:49 +0800

    Test

--- mirror.sh
+++ mirror.sh
@@ -5,5 +5,6 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
+git fetch
 git checkout origin/develop
 git push -u mirror develop
