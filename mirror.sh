dd2ffe970c3d6de0d69131282442546c53e3b013
Tik <tikman@increact.com>
2021-07-14 10:36:31 +0800

    Check whoami

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,7 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+whoami
 cd "$INPUT_PATH"
 git fetch
 git checkout develop
