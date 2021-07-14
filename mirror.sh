fdfceaa0fa40afc386a5be629276849200086fa4
Tik <tikman@increact.com>
2021-07-14 10:42:52 +0800

    su centos

--- mirror.sh
+++ mirror.sh
@@ -4,7 +4,7 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-whoami
+su centos
 cd "$INPUT_PATH"
 git fetch
 git checkout develop
